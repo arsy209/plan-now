69e8e8570cc8cc6490e29385c6725ab121b6a2a7
Arshdeep Singh <singh_arsy@yahoo.com>
2016-12-29 21:07:22 -0500

    minor changes to edit and register files for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ include InvitationsHelper
 
 protected
   def configure_permitted_parameters
-       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+       devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:phone])
   end
 end
