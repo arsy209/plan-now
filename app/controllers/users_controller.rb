75b7437fe22eb14c94008a2616d1c94cfc84f8a4
Arshdeep Singh <singh_arsy@yahoo.com>
2016-12-30 21:23:54 -0500

    users can now edit their account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @invites = user_invitations(current_user).sort_by(&:date)
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    end
+  end
 end
