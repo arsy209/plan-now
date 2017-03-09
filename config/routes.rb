e5e3dddcd84bcfdd887f2ab06f3d9e788467bac9
Arshdeep Singh <singh_arsy@yahoo.com>
2017-03-09 12:53:14 -0500

    fix spacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'events#index'
-
   resources :users, only: [:index, :show]
-
   resources :comments, only: [:create, :update, :destroy]
-
   resources :events do
     resources :invitations
   end
-
   resources :attendings, only: [:create, :update]
 end
